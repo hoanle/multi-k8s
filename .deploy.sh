8d9b7200a34f94fef74b9ae652e8119aa0938f85
Le Hoan <hoan.le@dpm.today>
2021-07-11 04:18:39 +0800

    fix wrong texts

--- .deploy.sh
+++ .deploy.sh
@@ -3,16 +3,15 @@ docker build -t hoanlezuhlke/multi-d1-server:latest -t hoanlezuhlke/multi-d1-ser
 docker build -t hoanlezuhlke/multi-d1-worker:latest -t hoanlezuhlke/multi-d1-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push hoanlezuhlke/multi-d1-client:latest
-docker push hoanlezuhlke/multi-d1-client:$SHA
-
 docker push hoanlezuhlke/multi-d1-server:latest
-docker push hoanlezuhlke/multi-d1-server:$SHA
-
 docker push hoanlezuhlke/multi-d1-worker:latest
+
+docker push hoanlezuhlke/multi-d1-client:$SHA
+docker push hoanlezuhlke/multi-d1-server:$SHA
 docker push hoanlezuhlke/multi-d1-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=hoanlezuhlke/multi-d1-server:$SHA
 kubectl set image deployments/client-deployment server=hoanlezuhlke/multi-d1-client:$SHA
-kubectl set image deployments/worker-deployment server=hoanlezuhlke/multi-d1-worker:$SHA
+kubectl set image deployments/server-deployment client=hoanlezuhlke/multi-d1-server:$SHA
+kubectl set image deployments/worker-deployment worker=hoanlezuhlke/multi-d1-worker:$SHA
